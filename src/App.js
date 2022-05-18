e545c92292d1ebae4f49dda58a1a4bb9e5c15987
Mikhail Kozachenko <kozachenko.mikhail@softkitit.com>
2022-05-18 17:09:09 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Logo from './assets/images/free-png.ru-740-450x200.png';
+
+const App = () => {
+  return (
+    <div className="App" style={{ background: '#ce3ce3' }}>
+      hello
+      <img src={Logo} alt="logo" />
+    </div>
+  );
+};
+
+export default App;
